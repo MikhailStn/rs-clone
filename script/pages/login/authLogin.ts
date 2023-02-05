f7d68727f37b492141d779f9d274e2450d773e8f
Mikhail Stankevich <mikhailstn1@gmail.com>
2023-02-05 07:12:49 +0300

    feat: add authorisation and login

--- script/pages/login/authLogin.ts
+++ script/pages/login/authLogin.ts
@@ -1,65 +1,119 @@
 import { createHtmlElement } from "../../utils";
 
-const loginSection = createHtmlElement('section', 'login-section');
+const loginSection = createHtmlElement("section", "login-section");
 
-function renderLoginPage(){
-  loginSection.innerHTML = '';
-  const loginLayout = createHtmlElement('div', 'login-layout');
+function renderLoginPage() {
+  loginSection.innerHTML = "";
+  const loginLayout = createHtmlElement("div", "login-layout");
   loginSection.append(loginLayout);
-  const loginBlockWrapper = createHtmlElement('div', 'login-block-wrapper');
+  const loginBlockWrapper = createHtmlElement("div", "login-block-wrapper");
   loginLayout.append(loginBlockWrapper);
-  const titleLogin = createHtmlElement('h1', 'login-title', '', 'Petsy');
+  const titleLogin = createHtmlElement("h1", "login-title", "", "Petsy");
   loginBlockWrapper.append(titleLogin);
-  const textAfterTitleLogin = createHtmlElement('div', 'text-after-title-login','', 'Welcome back!');
+  const textAfterTitleLogin = createHtmlElement(
+    "div",
+    "text-after-title-login",
+    "",
+    "Welcome back!"
+  );
   loginBlockWrapper.append(textAfterTitleLogin);
-  const inputLoginEmail = createHtmlElement('input', 'input-login-email input-login');
-  const inputLoginPassword = createHtmlElement('input', 'input-login-password input-login');
+  const inputLoginEmail = createHtmlElement(
+    "input",
+    "input-login-email input-login"
+  ) as HTMLInputElement;
+  const inputLoginPassword = createHtmlElement(
+    "input",
+    "input-login-password input-login"
+  ) as HTMLInputElement;
   loginBlockWrapper.append(inputLoginEmail);
   loginBlockWrapper.append(inputLoginPassword);
-  const emailTextError = createHtmlElement('p', 'email-text-error-login', '', 'Incorrect email format');
+  const emailTextError = createHtmlElement(
+    "p",
+    "email-text-error-login",
+    "",
+    "Incorrect email format"
+  );
   inputLoginEmail.after(emailTextError);
-  const passwordTextError = createHtmlElement('p', 'password-text-error-login', '', 'Enter your password');
+  const passwordTextError = createHtmlElement(
+    "p",
+    "password-text-error-login",
+    "",
+    "Enter your password"
+  );
   inputLoginPassword.after(passwordTextError);
-  const btnLogin = createHtmlElement('button', 'btn-login', '', 'Log in');
-  if(btnLogin instanceof HTMLButtonElement){
-    btnLogin.type = 'submit';
+  const btnLogin = createHtmlElement("button", "btn-login", "", "Log in");
+  if (btnLogin instanceof HTMLButtonElement) {
+    btnLogin.type = "submit";
   }
   loginBlockWrapper.append(btnLogin);
-  if(inputLoginEmail instanceof HTMLInputElement){
-    inputLoginEmail.type = 'email';
-    inputLoginEmail.placeholder = 'E-mail';
+  btnLogin.addEventListener("click", () => {
+    const fetchData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email: `${inputLoginEmail.value}`,
+        password: `${inputLoginPassword.value}`,
+      }),
+    };
+    fetch(`http://localhost:5000/auth/login`, fetchData)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("curr-user-id", `${data.id}`);
+        console.log(localStorage.getItem("curr-user-id"));
+      });
+  });
+  if (inputLoginEmail instanceof HTMLInputElement) {
+    inputLoginEmail.type = "email";
+    inputLoginEmail.placeholder = "E-mail";
     inputLoginEmail.setAttribute("required", "");
-    btnLogin.addEventListener('click', ()=>{
-        if(!inputLoginEmail.checkValidity() && !emailTextError.classList.contains('active')){
-            emailTextError.classList.add('active');
-        }else if(inputLoginEmail.checkValidity() && emailTextError.classList.contains('active')){
-            emailTextError.classList.remove('active');
-            inputLoginEmail.value = '';
-        }
-      })
-    }
-    if(inputLoginPassword instanceof HTMLInputElement){
-      inputLoginPassword.type = 'text';
-      inputLoginPassword.setAttribute("required", "");
-      inputLoginPassword.placeholder = 'Password';
-      btnLogin.addEventListener('click', ()=>{
-          if(!inputLoginPassword.checkValidity() && !passwordTextError.classList.contains('active')){
-            passwordTextError.classList.add('active');
-          }else if(inputLoginPassword.checkValidity() && passwordTextError.classList.contains('active')){
-            passwordTextError.classList.remove('active');
-          }
-      })
-    }
-  const registText = createHtmlElement('div', 'registration-text-login');
-  registText.innerHTML = '<p>Would you like to join Petsy? <a class = "link-for-registr-in-login" href = "/auth/register/owner">Create an account</a></p>';
+    btnLogin.addEventListener("click", () => {
+      if (
+        !inputLoginEmail.checkValidity() &&
+        !emailTextError.classList.contains("active")
+      ) {
+        emailTextError.classList.add("active");
+      } else if (
+        inputLoginEmail.checkValidity() &&
+        emailTextError.classList.contains("active")
+      ) {
+        emailTextError.classList.remove("active");
+        inputLoginEmail.value = "";
+      }
+    });
+  }
+  if (inputLoginPassword instanceof HTMLInputElement) {
+    inputLoginPassword.type = "text";
+    inputLoginPassword.setAttribute("required", "");
+    inputLoginPassword.placeholder = "Password";
+    btnLogin.addEventListener("click", () => {
+      if (
+        !inputLoginPassword.checkValidity() &&
+        !passwordTextError.classList.contains("active")
+      ) {
+        passwordTextError.classList.add("active");
+      } else if (
+        inputLoginPassword.checkValidity() &&
+        passwordTextError.classList.contains("active")
+      ) {
+        passwordTextError.classList.remove("active");
+      }
+    });
+  }
+  const registText = createHtmlElement("div", "registration-text-login");
+  registText.innerHTML =
+    '<p>Would you like to join Petsy? <a class = "link-for-registr-in-login" href = "/auth/register/owner">Create an account</a></p>';
   loginBlockWrapper.append(registText);
   return loginSection;
 }
 
-
 export default function authLogin() {
-  document.body.innerHTML = ''
-    renderLoginPage();
-    return document.body.append(loginSection);
-
+  document.body.innerHTML = "";
+  renderLoginPage();
+  return document.body.append(loginSection);
 }
--- script/pages/login/authRegistration.ts
+++ script/pages/login/authRegistration.ts
@@ -1,98 +1,217 @@
 import { createHtmlElement } from "../../utils";
-const registrationSection = createHtmlElement('section', 'registration-section');
+const registrationSection = createHtmlElement(
+  "section",
+  "registration-section"
+);
 
-function renderRegistrPage(role: string){
-    registrationSection.innerHTML = '';
-    const regitrBlockWrapper = createHtmlElement('div', 'registration-block-wrapper');
-    registrationSection.append(regitrBlockWrapper);
-    const imageRegistWrap = createHtmlElement('div', 'img-registration-wrapper');
-    const imgRegitration = createHtmlElement('img', 'img-registration');
-    if(imgRegitration instanceof HTMLImageElement){
-      role === 'owner'? imgRegitration.src = 'https://petsy.pl/_next/image/?url=%2Fimages%2Fregister_owner.jpg&w=1920&q=75':imgRegitration.src = 'https://petsy.pl/_next/image/?url=%2Fimages%2Fregister_petsitter.jpg&w=1920&q=75';
+function renderRegistrPage(role: string) {
+  registrationSection.innerHTML = "";
+  const regitrBlockWrapper = createHtmlElement(
+    "div",
+    "registration-block-wrapper"
+  );
+  registrationSection.append(regitrBlockWrapper);
+  const imageRegistWrap = createHtmlElement("div", "img-registration-wrapper");
+  const imgRegitration = createHtmlElement("img", "img-registration");
+  if (imgRegitration instanceof HTMLImageElement) {
+    role === "owner"
+      ? (imgRegitration.src =
+          "https://petsy.pl/_next/image/?url=%2Fimages%2Fregister_owner.jpg&w=1920&q=75")
+      : (imgRegitration.src =
+          "https://petsy.pl/_next/image/?url=%2Fimages%2Fregister_petsitter.jpg&w=1920&q=75");
+  }
+  regitrBlockWrapper.append(imageRegistWrap);
+  imageRegistWrap.append(imgRegitration);
+  const blockInputsAndText = createHtmlElement(
+    "div",
+    "block-inputs-text-registration"
+  );
+  regitrBlockWrapper.append(blockInputsAndText);
+  const linkOnMain = createHtmlElement(
+    "a",
+    "link-on-main-from-registration",
+    "",
+    "Back to home page"
+  );
+  blockInputsAndText.append(linkOnMain);
+  const textForRegistrRole = createHtmlElement(
+    "h1",
+    "text-for-registration-role"
+  );
+  role === "owner"
+    ? (textForRegistrRole.innerHTML =
+        "Time for you and the best care for your pet")
+    : (textForRegistrRole.innerHTML =
+        "Become a petsitter. Earn by spending time among animals");
+  blockInputsAndText.append(textForRegistrRole);
+  const textRegistrLike = createHtmlElement(
+    "div",
+    "text-registration-as",
+    "",
+    "Register as:"
+  );
+  blockInputsAndText.append(textRegistrLike);
+  const blockLinkRole = createHtmlElement(
+    "div",
+    "block-link-role-registration"
+  );
+  blockInputsAndText.append(blockLinkRole);
+  const linkOwner = createHtmlElement("a", "", "", "Pet owner");
+  role === "owner"
+    ? (linkOwner.className = "link-owner-registration active")
+    : (linkOwner.className = "link-owner-registration");
+  blockLinkRole.append(linkOwner);
+  const linkPetsit = createHtmlElement(
+    "a",
+    "link-petsitter-registration",
+    "",
+    "Petsitter"
+  );
+  role === "owner"
+    ? (linkPetsit.className = "link-petsitter-registration")
+    : (linkPetsit.className = "link-petsitter-registration active");
+  blockLinkRole.append(linkPetsit);
+  if (
+    linkPetsit instanceof HTMLAnchorElement &&
+    linkOwner instanceof HTMLAnchorElement &&
+    linkOnMain instanceof HTMLAnchorElement
+  ) {
+    linkOnMain.href = "/";
+    linkPetsit.href = "/auth/register/petsitter";
+    linkOwner.href = "/auth/register/owner";
+  }
+  const inputPlaceOfLive = createHtmlElement(
+    "input",
+    "input-place-of-live input-registration",
+    "city"
+  ) as HTMLInputElement;
+  blockInputsAndText.append(inputPlaceOfLive);
+  if (inputPlaceOfLive instanceof HTMLInputElement) {
+    inputPlaceOfLive.type = "text";
+    inputPlaceOfLive.placeholder = "City";
+  }
+  const blockNameInputs = createHtmlElement("div", "block-names-inputs");
+  blockInputsAndText.append(blockNameInputs);
+  const inputFirstName = createHtmlElement(
+    "input",
+    "input-firstname input-registration",
+    "firstname"
+  ) as HTMLInputElement;
+  blockNameInputs.append(inputFirstName);
+  if (inputFirstName instanceof HTMLInputElement) {
+    inputFirstName.type = "text";
+    inputFirstName.placeholder = "Firstname";
+  }
+  const inputLastName = createHtmlElement(
+    "input",
+    "input-lastname input-registration",
+    "lastname"
+  ) as HTMLInputElement;
+  blockNameInputs.append(inputLastName);
+  if (inputLastName instanceof HTMLInputElement) {
+    inputLastName.type = "text";
+    inputLastName.placeholder = "Lastname";
+  }
+  const inputNumber = createHtmlElement(
+    "input",
+    "input-phone-number input-registration",
+    "phone-number"
+  ) as HTMLInputElement;
+  blockInputsAndText.append(inputNumber);
+  if (inputNumber instanceof HTMLInputElement) {
+    inputNumber.type = "text";
+    inputNumber.placeholder = "Phone number";
+  }
+  const emailInput = createHtmlElement(
+    "input",
+    "input-email-registration input-registration",
+    "email"
+  ) as HTMLInputElement;
+  blockInputsAndText.append(emailInput);
+  if (emailInput instanceof HTMLInputElement) {
+    emailInput.type = "email";
+    emailInput.placeholder = "E-mail";
+  }
+  const inputPassword = createHtmlElement(
+    "input",
+    "input-password-registration input-registration",
+    "password"
+  ) as HTMLInputElement;
+  blockInputsAndText.append(inputPassword);
+  if (inputPassword instanceof HTMLInputElement) {
+    inputPassword.type = "text";
+    inputPassword.placeholder = "Password";
+  }
+  const inputSecondPassword = createHtmlElement(
+    "input",
+    "input-repeat-password-registration input-registration",
+    "repeat-password"
+  ) as HTMLInputElement;
+  blockInputsAndText.append(inputSecondPassword);
+  if (inputSecondPassword instanceof HTMLInputElement) {
+    inputSecondPassword.type = "text";
+    inputSecondPassword.placeholder = "Repeat password";
+  }
+  const btnRegistration = createHtmlElement(
+    "button",
+    "btn-registration",
+    "",
+    "Register as a pet owner"
+  );
+  role === "owner"
+    ? ((btnRegistration.innerHTML = "Register as a pet owner"),
+      (btnRegistration.id = "registration-pet-owner"))
+    : ((btnRegistration.innerHTML = "Register as a petsitter"),
+      (btnRegistration.id = "registration-petsitter"));
+  blockInputsAndText.append(btnRegistration);
+  if (btnRegistration instanceof HTMLButtonElement) {
+    btnRegistration.type = "submit";
+  }
+  btnRegistration.addEventListener("click", () => {
+    let link = "";
+    let role = "";
+    if (btnRegistration.id == "registration-pet-owner") {
+      link = "http://localhost:5000/auth/register/owner";
+      role = "OWNER";
+    } else if (btnRegistration.id == "registration-petsitter") {
+      link = "http://localhost:5000/auth/register/petsitter";
+      role = "PETSITTER";
     }
-    regitrBlockWrapper.append(imageRegistWrap);
-    imageRegistWrap.append(imgRegitration);
-    const blockInputsAndText = createHtmlElement('div', 'block-inputs-text-registration');
-    regitrBlockWrapper.append(blockInputsAndText);
-    const linkOnMain = createHtmlElement('a', 'link-on-main-from-registration', '','Back to home page');
-    blockInputsAndText.append(linkOnMain);
-    const textForRegistrRole = createHtmlElement('h1', 'text-for-registration-role');
-    role === 'owner'? textForRegistrRole.innerHTML = 'Time for you and the best care for your pet': textForRegistrRole.innerHTML = 'Become a petsitter. Earn by spending time among animals';
-    blockInputsAndText.append(textForRegistrRole);
-    const textRegistrLike = createHtmlElement('div', 'text-registration-as','','Register as:');
-    blockInputsAndText.append(textRegistrLike);
-    const blockLinkRole = createHtmlElement('div', 'block-link-role-registration');
-    blockInputsAndText.append(blockLinkRole);
-    const linkOwner = createHtmlElement('a', '', '', 'Pet owner');
-    role === 'owner'? linkOwner.className = 'link-owner-registration active' : linkOwner.className = 'link-owner-registration';
-    blockLinkRole.append(linkOwner);
-    const linkPetsit = createHtmlElement('a', 'link-petsitter-registration', '', 'Petsitter');
-    role === 'owner'? linkPetsit.className = 'link-petsitter-registration' : linkPetsit.className = 'link-petsitter-registration active';
-    blockLinkRole.append(linkPetsit);
-    if(linkPetsit instanceof HTMLAnchorElement && linkOwner instanceof HTMLAnchorElement && linkOnMain instanceof HTMLAnchorElement){
-        linkOnMain.href = '/';
-        linkPetsit.href = '/auth/register/petsitter';
-        linkOwner.href = '/auth/register/owner';
+    if (inputPassword.value == inputSecondPassword.value) {
+      const fecthData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          firstName: `${inputFirstName.value}`,
+          lastName: `${inputLastName.value}`,
+          city: `${inputPlaceOfLive.value}`,
+          email: `${emailInput.value}`,
+          password: `${inputPassword.value}`,
+          phone: Number(inputNumber.value),
+          role: role,
+          pets: [],
+        }),
+      };
+      fetch(`${link}`, fecthData)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          localStorage.setItem("curr-user-id", `${data.id}`)
+          console.log(localStorage.getItem("curr-user-id"));
+        });
+    } else {
+      console.log("Пароли не совпадают");
     }
-    const inputPlaceOfLive = createHtmlElement('input', 'input-place-of-live input-registration','city');
-    blockInputsAndText.append(inputPlaceOfLive);
-    if(inputPlaceOfLive instanceof HTMLInputElement){
-      inputPlaceOfLive.type = 'text';
-      inputPlaceOfLive.placeholder = 'City';
-    }
-    const blockNameInputs = createHtmlElement('div', 'block-names-inputs');
-    blockInputsAndText.append(blockNameInputs);
-    const inputFirstName = createHtmlElement('input', 'input-firstname input-registration','firstname');
-    blockNameInputs.append(inputFirstName);
-    if(inputFirstName instanceof HTMLInputElement){
-      inputFirstName.type = 'text';
-      inputFirstName.placeholder = 'Firstname';
-    }
-    const inputLastName = createHtmlElement('input', 'input-lastname input-registration','lastname');
-    blockNameInputs.append(inputLastName);
-    if(inputLastName instanceof HTMLInputElement){
-      inputLastName.type = 'text';
-      inputLastName.placeholder = 'Lastname';
-    }
-    const inputNumber = createHtmlElement('input', 'input-phone-number input-registration','phone-number');
-    blockInputsAndText.append(inputNumber);
-    if(inputNumber instanceof HTMLInputElement){
-      inputNumber.type = 'text';
-      inputNumber.placeholder = 'Phone number';
-    }
-    const emailInput = createHtmlElement('input', 'input-email-registration input-registration', 'email');
-    blockInputsAndText.append(emailInput);
-    if(emailInput instanceof HTMLInputElement){
-      emailInput.type = 'email';
-      emailInput.placeholder = 'E-mail';
-    }
-    const inputPassword = createHtmlElement('input', 'input-password-registration input-registration', 'password');
-    blockInputsAndText.append(inputPassword);
-    if(inputPassword instanceof HTMLInputElement){
-      inputPassword.type = 'text';
-      inputPassword.placeholder = 'Password';
-    }
-    const inputSecondPassword = createHtmlElement('input', 'input-repeat-password-registration input-registration', 'repeat-password');
-    blockInputsAndText.append(inputSecondPassword);
-    if(inputSecondPassword instanceof HTMLInputElement){
-      inputSecondPassword.type = 'text';
-      inputSecondPassword.placeholder = 'Repeat password';
-    }
-    const btnRegistration = createHtmlElement('button', 'btn-registration','', 'Register as a pet owner');
-    role === "owner"? (
-      btnRegistration.innerHTML = 'Register as a pet owner',
-      btnRegistration.id = "registration-pet-owner"):(
-        btnRegistration.innerHTML = 'Register as a petsitter',
-        btnRegistration.id = 'registration-petsitter');
-    blockInputsAndText.append(btnRegistration);
-    if(btnRegistration instanceof HTMLButtonElement){
-        btnRegistration.type = 'submit';
-    }
-    const textQuestion = createHtmlElement('div', 'text-question-registration');
-    textQuestion.innerHTML = '<p>Already have an account on Petsy?<a class="link-sign-in-registration" href = "/auth/login">Sign in</a></p>';
-    blockInputsAndText.append(textQuestion);
-
-
+  });
+  const textQuestion = createHtmlElement("div", "text-question-registration");
+  textQuestion.innerHTML =
+    '<p>Already have an account on Petsy?<a class="link-sign-in-registration" href = "/auth/login">Sign in</a></p>';
+  blockInputsAndText.append(textQuestion);
 }
 
 export default function authRegister() {
@@ -100,7 +219,7 @@ export default function authRegister() {
   if (main) {
     console.log(window.location.pathname);
     const path = window.location.pathname;
-    const role = path.split('/')[3];
+    const role = path.split("/")[3];
     main.innerHTML = "";
     renderRegistrPage(role);
     return main?.append(registrationSection);
