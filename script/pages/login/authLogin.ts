e7b22d5b1ca143d0b6643f17c8701b6922362b24
LisaDobruk <liskam8525gmail.com>
2023-02-06 16:01:32 +0100

    feat: add function for POST data

--- script/pages/login/authLogin.ts
+++ script/pages/login/authLogin.ts
@@ -1,9 +1,8 @@
 import { createHtmlElement } from "../../utils";
-import { handleFormAfterSubmit } from "../../commonFunction/formFunction";
 
 const loginSection = createHtmlElement('section', 'login-section');
 
-function renderLoginPage(){
+function renderLoginPage() {
   loginSection.innerHTML = '';
   const loginLayout = createHtmlElement('div', 'login-layout');
   loginSection.append(loginLayout);
@@ -11,12 +10,12 @@ function renderLoginPage(){
   loginLayout.append(loginBlockWrapper);
   const titleLogin = createHtmlElement('h1', 'login-title', '', 'Petsi');
   loginBlockWrapper.append(titleLogin);
-  const textAfterTitleLogin = createHtmlElement('div', 'text-after-title-login','', 'Welcome back!');
+  const textAfterTitleLogin = createHtmlElement('div', 'text-after-title-login', '', 'Welcome back!');
   loginBlockWrapper.append(textAfterTitleLogin);
   const formLogin = createHtmlElement('form', 'form-login', 'form-login') as HTMLFormElement;
   loginBlockWrapper.append(formLogin);
-  const inputLoginEmail = createHtmlElement('input', 'input-login-email input-login');
-  const inputLoginPassword = createHtmlElement('input', 'input-login-password input-login');
+  const inputLoginEmail = createHtmlElement('input', 'input-login-email input-login') as HTMLInputElement;
+  const inputLoginPassword = createHtmlElement('input', 'input-login-password input-login') as HTMLInputElement;
   formLogin.append(inputLoginEmail);
   formLogin.append(inputLoginPassword);
   const emailTextError = createHtmlElement('p', 'email-text-error-login', 'email-error', 'Incorrect email format');
@@ -24,51 +23,69 @@ function renderLoginPage(){
   const passwordTextError = createHtmlElement('p', 'password-text-error-login', 'password-error', 'Enter your password');
   inputLoginPassword.after(passwordTextError);
   const btnLogin = createHtmlElement('button', 'btn-login', '', 'Log in');
-  if(btnLogin instanceof HTMLButtonElement){
+  if (btnLogin instanceof HTMLButtonElement) {
     btnLogin.type = 'submit';
   }
   formLogin.append(btnLogin);
-  if(inputLoginEmail instanceof HTMLInputElement){
+  btnLogin.addEventListener("click", () => {
+    const fetchData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email: `${inputLoginEmail.value}`,
+        password: `${inputLoginPassword.value}`,
+      }),
+    };
+    fetch(`http://localhost:5000/auth/login`, fetchData)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("curr-user-id", `${data.id}`);
+        console.log(localStorage.getItem("curr-user-id"));
+      });
+  });
+
     inputLoginEmail.type = 'email';
     inputLoginEmail.name = 'email';
     inputLoginEmail.placeholder = 'E-mail';
     inputLoginEmail.setAttribute("required", "");
-    inputLoginEmail.addEventListener('input',()=>{
-      if (inputLoginEmail.checkValidity() && emailTextError.classList.contains('active')){
+    inputLoginEmail.addEventListener('input', () => {
+      if (inputLoginEmail.checkValidity() && emailTextError.classList.contains('active')) {
         emailTextError.classList.remove('active');
       }
     })
-    btnLogin.addEventListener('click', ()=>{
-        if(!inputLoginEmail.checkValidity() && !emailTextError.classList.contains('active')){
-            emailTextError.classList.add('active');
-        }else if(inputLoginEmail.checkValidity() && emailTextError.classList.contains('active')){
-            emailTextError.classList.remove('active');
-            inputLoginEmail.value = '';
-        }
-      })
-    }
-    if(inputLoginPassword instanceof HTMLInputElement){
-      inputLoginPassword.type = 'password';
-      inputLoginPassword.name = 'password';
-      inputLoginPassword.setAttribute("required", "");
-      inputLoginPassword.placeholder = 'Password';
-      inputLoginPassword.addEventListener('input', ()=>{
-        if (inputLoginPassword.checkValidity() && passwordTextError.classList.contains('active')){
-          passwordTextError.classList.remove('active');
-        }
-      })
-      btnLogin.addEventListener('click', ()=>{
-          if(!inputLoginPassword.checkValidity() && !passwordTextError.classList.contains('active')){
-            passwordTextError.classList.add('active');
-          }else if(inputLoginPassword.checkValidity() && passwordTextError.classList.contains('active')){
-            passwordTextError.classList.remove('active');
-          }
-      })
-    }
+    btnLogin.addEventListener('click', () => {
+      if (!inputLoginEmail.checkValidity() && !emailTextError.classList.contains('active')) {
+        emailTextError.classList.add('active');
+      } else if (inputLoginEmail.checkValidity() && emailTextError.classList.contains('active')) {
+        emailTextError.classList.remove('active');
+        inputLoginEmail.value = '';
+      }
+    })
 
-    formLogin.addEventListener('submit', (event)=>{
-      handleFormAfterSubmit(event, formLogin);
-    });
+
+    inputLoginPassword.type = 'password';
+    inputLoginPassword.name = 'password';
+    inputLoginPassword.setAttribute("required", "");
+    inputLoginPassword.placeholder = 'Password';
+    inputLoginPassword.addEventListener('input', () => {
+      if (inputLoginPassword.checkValidity() && passwordTextError.classList.contains('active')) {
+        passwordTextError.classList.remove('active');
+      }
+    })
+    btnLogin.addEventListener('click', () => {
+      if (!inputLoginPassword.checkValidity() && !passwordTextError.classList.contains('active')) {
+        passwordTextError.classList.add('active');
+      } else if (inputLoginPassword.checkValidity() && passwordTextError.classList.contains('active')) {
+        passwordTextError.classList.remove('active');
+      }
+    })
+
   const registText = createHtmlElement('div', 'registration-text-login');
   registText.innerHTML = '<p>Would you like to join Petsi? <a class = "link-for-registr-in-login" href = "/auth/register/owner">Create an account</a></p>';
   loginBlockWrapper.append(registText);
@@ -78,7 +95,7 @@ function renderLoginPage(){
 
 export default function authLogin() {
   document.body.innerHTML = ''
-    renderLoginPage();
-    return document.body.append(loginSection);
+  renderLoginPage();
+  return document.body.append(loginSection);
 
 }
--- script/pages/login/authRegistration.ts
+++ script/pages/login/authRegistration.ts
@@ -38,111 +38,154 @@ function renderRegistrPage(role: string) {
     linkPetsit.href = '/auth/register/petsitter';
     linkOwner.href = '/auth/register/owner';
   }
-  const formRegistration = createHtmlElement('form','registration-form', `registation-form-${role}`);
+  const formRegistration = createHtmlElement('form', 'registration-form', `registation-form-${role}`);
   blockInputsAndText.append(formRegistration);
-  const inputPlaceOfLive = createHtmlElement('input', 'input-place-of-live input-registration', 'city');
-  const dataListCity = createHtmlElement('datalist','','city-variable') as HTMLDListElement;
-  dataListCity.innerHTML= '<option value="Minsk"></option><option value="Brest"></option><option value="Vitebsk"></option><option value="Grodno"></option><option value="Gomel"></option><option value="Mogilev"></option>';
+
+  const inputPlaceOfLive = createHtmlElement('input', 'input-place-of-live input-registration', 'city') as HTMLInputElement;
+  const dataListCity = createHtmlElement('datalist', '', 'city-variable') as HTMLDListElement;
+  dataListCity.innerHTML = '<option value="Minsk"></option><option value="Brest"></option><option value="Vitebsk"></option><option value="Grodno"></option><option value="Gomel"></option><option value="Mogilev"></option>';
   const placeTextError = createHtmlElement('p', 'city-text-error-registration text-error-registration', 'city-error', 'Enter city from list');
   formRegistration.append(inputPlaceOfLive);
   inputPlaceOfLive.after(dataListCity);
   inputPlaceOfLive.after(placeTextError);
-  if (inputPlaceOfLive instanceof HTMLInputElement) {
-    inputPlaceOfLive.type = 'text';
-    inputPlaceOfLive.name = 'af2Km9q';
-    inputPlaceOfLive.placeholder = 'City';
-    inputPlaceOfLive.pattern = "[Mm]insk|[Bb]rest|[Vv]itebsk|[Gg]rodno|[Gg]omel|[Mm]ogilev";
-    inputPlaceOfLive.setAttribute("list", "city-variable");
-    inputPlaceOfLive.setAttribute("autocomplete","off");
-    inputPlaceOfLive.setAttribute("required", "");
-  }
+  inputPlaceOfLive.type = 'text';
+  inputPlaceOfLive.name = 'af2Km9q';
+  inputPlaceOfLive.placeholder = 'City';
+  inputPlaceOfLive.pattern = "[Mm]insk|[Bb]rest|[Vv]itebsk|[Gg]rodno|[Gg]omel|[Mm]ogilev";
+  inputPlaceOfLive.setAttribute("list", "city-variable");
+  inputPlaceOfLive.setAttribute("autocomplete", "off");
+  inputPlaceOfLive.setAttribute("required", "");
   inputPlaceOfLive.addEventListener('input', checkValidity);
+
   const blockNameInputs = createHtmlElement('div', 'block-names-inputs');
   formRegistration.append(blockNameInputs);
-  const inputFirstName = createHtmlElement('input', 'input-firstname input-registration', 'firstname');
+  const inputFirstName = createHtmlElement('input', 'input-firstname input-registration', 'firstname') as HTMLInputElement;
   const firstNameTextError = createHtmlElement('p', 'first-name-text-error-registration text-error-registration', 'first-name-error', 'Enter your name');
   blockNameInputs.append(inputFirstName);
   inputFirstName.after(firstNameTextError);
-  if (inputFirstName instanceof HTMLInputElement) {
-    inputFirstName.type = 'text';
-    inputFirstName.placeholder = 'Firstname';
-    inputFirstName.name = 'firstname';
-    inputFirstName.setAttribute("required", "");
-  }
+  inputFirstName.type = 'text';
+  inputFirstName.placeholder = 'Firstname';
+  inputFirstName.name = 'firstname';
+  inputFirstName.setAttribute("required", "");
+
   inputFirstName.addEventListener('input', checkValidity);
-  const inputLastName = createHtmlElement('input', 'input-lastname input-registration', 'lastname');
+  const inputLastName = createHtmlElement('input', 'input-lastname input-registration', 'lastname') as HTMLInputElement;
   const lastNameTextError = createHtmlElement('p', 'last-name-text-error-registration text-error-registration', 'last-name-error', 'Enter your lastname');
   blockNameInputs.append(inputLastName);
   inputLastName.after(lastNameTextError);
-  if (inputLastName instanceof HTMLInputElement) {
-    inputLastName.type = 'text';
-    inputLastName.placeholder = 'Lastname';
-    inputLastName.name = 'lastname';
-    inputLastName.setAttribute("required", "");
-  }
+  inputLastName.type = 'text';
+  inputLastName.placeholder = 'Lastname';
+  inputLastName.name = 'lastname';
+  inputLastName.setAttribute("required", "");
   inputLastName.addEventListener('input', checkValidity);
-  const inputNumber = createHtmlElement('input', 'input-phone-number input-registration', 'phone-number');
+
+  const inputNumber = createHtmlElement('input', 'input-phone-number input-registration', 'phone-number') as HTMLInputElement;
   const phoneTextError = createHtmlElement('p', 'phone-text-error-registration text-error-registration', 'phone-error', 'Enter your correct phone');
   formRegistration.append(inputNumber);
   inputNumber.after(phoneTextError);
-  if (inputNumber instanceof HTMLInputElement) {
-    inputNumber.type = 'text';
-    inputNumber.placeholder = 'Phone number without "+"';
-    inputNumber.name = 'phone';
-    inputNumber.pattern = "^[0-9]{9,}$";
-    inputNumber.setAttribute("required", "");
-  }
+  inputNumber.type = 'text';
+  inputNumber.placeholder = 'Phone number without "+"';
+  inputNumber.name = 'phone';
+  inputNumber.pattern = "^[0-9]{9,}$";
+  inputNumber.setAttribute("required", "");
   inputNumber.addEventListener('input', checkValidity);
-  const emailInput = createHtmlElement('input', 'input-email-registration input-registration', 'email');
+
+  const emailInput = createHtmlElement('input', 'input-email-registration input-registration', 'email') as HTMLInputElement;
   const emailTextError = createHtmlElement('p', 'email-error-registration text-error-registration', 'email-error-registration', 'Enter your correct email');
   formRegistration.append(emailInput);
   emailInput.after(emailTextError);
-  if (emailInput instanceof HTMLInputElement) {
-    emailInput.type = 'email';
-    emailInput.placeholder = 'E-mail';
-    emailInput.name = 'email';
-    emailInput.setAttribute("required", "");
-  }
+  emailInput.type = 'email';
+  emailInput.placeholder = 'E-mail';
+  emailInput.name = 'email';
+  emailInput.setAttribute("required", "");
   emailInput.addEventListener('input', checkValidity);
+
   const inputPassword = createHtmlElement('input', 'input-password-registration input-registration', 'password') as HTMLInputElement;
   const passwordTextError = createHtmlElement('p', 'password-error-registration text-error-registration', 'password-error-registration', 'Your password must be 4 to 10 characters long');
   formRegistration.append(inputPassword);
   inputPassword.after(passwordTextError);
-  if (inputPassword instanceof HTMLInputElement) {
-    inputPassword.type = 'password';
-    inputPassword.placeholder = 'Password';
-    inputPassword.name = 'password';
-    inputPassword.pattern = "^(.){4,10}$";
-    inputPassword.setAttribute("required", "");
-  }
+  inputPassword.type = 'password';
+  inputPassword.placeholder = 'Password';
+  inputPassword.name = 'password';
+  inputPassword.pattern = "^(.){4,10}$";
+  inputPassword.setAttribute("required", "");
   inputPassword.addEventListener('input', checkValidity);
-  const inputSecondPassword = createHtmlElement('input', 'input-repeat-password-registration input-registration', 'repeat-password');
+
+  const inputSecondPassword = createHtmlElement('input', 'input-repeat-password-registration input-registration', 'repeat-password') as HTMLInputElement;
   const secondPasswordTextError = createHtmlElement('p', 'sec-passw-error-registration text-error-registration active', 'second-password-error', 'Your passwords don\'t match!');
   formRegistration.append(inputSecondPassword);
   inputSecondPassword.after(secondPasswordTextError);
   const btnRegistration = createHtmlElement('button', 'btn-registration', `btn-registration-${role}`) as HTMLButtonElement;
-  if (inputSecondPassword instanceof HTMLInputElement) {
-    inputSecondPassword.type = 'password';
-    inputSecondPassword.placeholder = 'Repeat password';
-    inputSecondPassword.name = 'repeatPassword';
-    inputSecondPassword.pattern = "^(.){4,10}$";
-    inputSecondPassword.setAttribute("required", "");
-    btnRegistration.addEventListener('click',(event)=>{
-      if(inputPassword.value !== inputSecondPassword.value){
-        event.preventDefault();
-        secondPasswordTextError.classList.remove("active");
-      }else{
-        secondPasswordTextError.classList.add("active");
-      }
-    })
-    inputSecondPassword.addEventListener('input',()=>{
-      if(inputPassword.value !== inputSecondPassword.value){
-        secondPasswordTextError.classList.remove("active");
-      }else{
-        secondPasswordTextError.classList.add("active");
-      }
-    })
-  }
+  inputSecondPassword.type = 'password';
+  inputSecondPassword.placeholder = 'Repeat password';
+  inputSecondPassword.name = 'repeatPassword';
+  inputSecondPassword.pattern = "^(.){4,10}$";
+  inputSecondPassword.setAttribute("required", "");
+  btnRegistration.addEventListener('click', (event) => {
+    if (inputPassword.value !== inputSecondPassword.value) {
+      event.preventDefault();
+      secondPasswordTextError.classList.remove("active");
+    } else {
+      secondPasswordTextError.classList.add("active");
+    }
+  })
+  inputSecondPassword.addEventListener('input', () => {
+    if (inputPassword.value !== inputSecondPassword.value) {
+      secondPasswordTextError.classList.remove("active");
+    } else {
+      secondPasswordTextError.classList.add("active");
+    }
+  })
+
+  btnRegistration.addEventListener("click", () => {
+    let link = "";
+    let role = "";
+    if (btnRegistration.id == "btn-registration-pet-owner") {
+      link = "http://localhost:5000/auth/register/owner";
+      role = "OWNER";
+    } else if (btnRegistration.id == "btn-registration-petsitter") {
+      link = "http://localhost:5000/auth/register/petsitter";
+      role = "PETSITTER";
+    }
+    if (inputPassword.value == inputSecondPassword.value) {
+      const fecthData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          firstName: `${inputFirstName.value}`,
+          lastName: `${inputLastName.value}`,
+          city: `${inputPlaceOfLive.value}`,
+          email: `${emailInput.value}`,
+          password: `${inputPassword.value}`,
+          phone: Number(inputNumber.value),
+          role: role,
+          pets: [],
+        }),
+      };
+      fetch(`${link}`, fecthData)
+        .then((response) => {
+          console.log(response.status);
+          if (response.status == 400) {
+            return response.json();
+          } else {
+            return;
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          localStorage.setItem("curr-user-id", `${data.id}`);
+          console.log(localStorage.getItem("curr-user-id"));
+          if (btnRegistration.id == "registration-petsitter") {
+            document.location.href = "/auth/register/form";
+          }
+        });
+    } else {
+      console.log("Пароли не совпадают");
+    }
+  });
   role === "owner" ? (
     btnRegistration.innerHTML = 'Register as a pet owner',
     btnRegistration.id = "registration-pet-owner") : (
@@ -153,7 +196,7 @@ function renderRegistrPage(role: string) {
     btnRegistration.type = 'submit';
     btnRegistration.disabled = true;
   }
-  formRegistration.addEventListener('input', (event)=>{
+  formRegistration.addEventListener('input', (event) => {
     checkValidityForm(event, btnRegistration);
   });
 
@@ -164,31 +207,30 @@ function renderRegistrPage(role: string) {
 
 function checkValidity(event: Event) {
   const input = event.target;
-    if(input instanceof HTMLInputElement && !input.checkValidity()){
-      if(input.nextElementSibling)input.nextElementSibling.classList.add('active');
-    }else if(input instanceof HTMLInputElement){
-      if(input.nextElementSibling)input.nextElementSibling.classList.remove('active');
-    }
-}
-function checkValidityForm(event: Event, button:HTMLButtonElement) {
-  if(event.target && event.target instanceof HTMLInputElement){
-    const formNode = event.target.form;
-    if(formNode){
-    const isValid = formNode.checkValidity();
-    button.disabled = !isValid;
+  if (input instanceof HTMLInputElement && !input.checkValidity()) {
+    if (input.nextElementSibling) input.nextElementSibling.classList.add('active');
+  } else if (input instanceof HTMLInputElement) {
+    if (input.nextElementSibling) input.nextElementSibling.classList.remove('active');
   }
 }
+function checkValidityForm(event: Event, button: HTMLButtonElement) {
+  if (event.target && event.target instanceof HTMLInputElement) {
+    const formNode = event.target.form;
+    if (formNode) {
+      const isValid = formNode.checkValidity();
+      button.disabled = !isValid;
+    }
+  }
 }
-
 
 export default function authRegister() {
-    document.body.innerHTML = '';
-    header(document.body);
-    const path = window.location.pathname;
-    const role = path.split('/')[3];
-    renderRegistrPage(role);
-    document.body.append(registrationSection);
-    footerFun(document.body);
-    return document.body;
-  }
+  document.body.innerHTML = '';
+  header(document.body);
+  const path = window.location.pathname;
+  const role = path.split('/')[3];
+  renderRegistrPage(role);
+  document.body.append(registrationSection);
+  footerFun(document.body);
+  return document.body;
+}
 
